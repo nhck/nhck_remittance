02d5511773ec1e3b522a21d0cd4195dee34e35d3
nhck <n@nhck.de>
2019-08-16 22:40:06 +0000

    Fixing Race condition. Added time feature.

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,5 @@
 const Remittance = artifacts.require("Remittance");
 
 module.exports = function(deployer) {
-  deployer.deploy(Remittance,true);
+  deployer.deploy(Remittance,true,10000);
 };
--- test/remittance.js
+++ test/remittance.js
@@ -1,29 +1,30 @@
-const { BN, soliditySha3 } = web3.utils;
+const {BN} = web3.utils;
 const RemittanceFactory = artifacts.require("Remittance");
 const assert = require("chai").assert;
 const truffleAssert = require('truffle-assertions');
 
 contract("Remittance", accounts => {
 
-    //Sender initates the contract
+    //sender initates the contract
     //exchange has Part One of the Password
     //receiver has Part Two of the Password
     const [coinbase, sender, exchange, thirdparty] = accounts;
 
     let remi;
 
-    const retrievalCode =  soliditySha3({type: "string", value: "Part One" + "Part Two"});
-    const retrievalCodeWrong =  soliditySha3({type: "string", value: "Wrong" + "Code"});
-    const retrievalCodeSecure =  soliditySha3({type: 'address', value: exchange}, {
-        type: 'bytes32',
-        value: retrievalCode
-    });
-
+    let retrievalCode;
+    let retrievalCodeWrong;
+    let retrievalCodeSecure;
 
     const testAmount = new BN(10);
 
+
     beforeEach('setup a new contract for each test', async function () {
-        remi = await RemittanceFactory.new(true, {from: sender});
+        remi = await RemittanceFactory.new(true, new BN(10000), {from: sender});
+
+        retrievalCode = await remi.createRetrievalCode("Part One" + "Part Two");
+        retrievalCodeWrong = await remi.createRetrievalCode("Wrong" + "Code");
+        retrievalCodeSecure = await remi.createRetrievalCodeSecure(retrievalCode, exchange);
     });
 
     it("should have an owner", async function () {
@@ -33,10 +34,16 @@ contract("Remittance", accounts => {
     describe("creating a test deposit", function () {
 
         let remiDepositTxObj;
+        let testTimestampNow;
+        let testTimestampFuture;
 
         beforeEach('make a deposit', async function () {
-
-            remiDepositTxObj = await remi.deposit(retrievalCodeSecure, exchange, {from: sender, value: testAmount});
+            testTimestampNow = new BN(Date.now()).div(new BN(1000));
+            testTimestampFuture = testTimestampNow.add(new BN(120));
+            remiDepositTxObj = await remi.deposit(retrievalCodeSecure, testTimestampFuture, {
+                from: sender,
+                value: testAmount
+            });
 
         });
 
@@ -46,13 +53,13 @@ contract("Remittance", accounts => {
                 let remiPayment = await remi.payments.call(retrievalCodeSecure);
 
                 assert.strictEqual("10", new BN(remiPayment.amount).toString(), "Deposited amount should be 10.");
-                assert.strictEqual(exchange, remiPayment.exchange, "Address of exchange should be correct.");
-                assert.strictEqual(sender, remiPayment.sender, "Address of sender should be recorded.")
+                assert.strictEqual(sender, remiPayment.sender, "Address of sender should be recorded.");
+                assert.strictEqual(testTimestampFuture.toString(), remiPayment.expiresTimestamp.toString(), "Expire timestamp should be set.");
 
                 assert.strictEqual(remiDepositTxObj.logs.length, 1, "Only one event is allowed in this transaction.");
 
                 await truffleAssert.eventEmitted(remiDepositTxObj, "LogDeposited", (ev) => {
-                    return ev.sender === sender && testAmount.eq(ev.amount) && ev.retrievalCode === retrievalCodeSecure;
+                    return ev.sender === sender && testAmount.eq(ev.amount) && ev.retrievalCodeSecure === retrievalCodeSecure;
                 });
             });
 
@@ -74,11 +81,28 @@ contract("Remittance", accounts => {
                 assert.strictEqual(remiWithdrawTxObj.logs.length, 1, "Only one event is allowed in this transaction.");
 
                 await truffleAssert.eventEmitted(remiWithdrawTxObj, "LogWithdrawn", (ev) => {
-                    return ev.sender === exchange && testAmount.eq(ev.amount) && ev.retrievalCode === retrievalCodeSecure;
+                    return ev.sender === exchange && testAmount.eq(ev.amount) && ev.retrievalCodeSecure === retrievalCodeSecure;
                 });
             });
 
             it("should allow reclaim", async function () {
+
+                //get the timedifference that is left and add 2000ms as grace period
+                let timeDiff = testTimestampFuture.mul(new BN(1000)).sub(new BN(Date.now())).add(new BN(2000));
+                //If there is a time difference left than we have to wait a bit and then make sure we have a fresh block
+                if (timeDiff.gt(new BN(0))) {
+                    //wait for the timedifference to pass
+                    await new Promise(resolve => {
+                        setTimeout(resolve, timeDiff);
+                    });
+
+                    let currentBlock = await web3.eth.getBlockNumber();
+                    //Wait for the next Block to be mined, after that proceed.
+                    while (currentBlock >= await web3.eth.getBlockNumber()) {
+                        setTimeout(function () {
+                        }, 1000);
+                    }
+                }
 
                 let remiPayment = await remi.payments.call(retrievalCodeSecure);
                 assert.strictEqual("10", new BN(remiPayment.amount).toString(), "Target amount should be 10.");
@@ -99,17 +123,43 @@ contract("Remittance", accounts => {
                 assert.strictEqual(remiReclaimTxObj.logs.length, 1, "Only one event is allowed in this transaction.");
 
                 await truffleAssert.eventEmitted(remiReclaimTxObj, "LogDepositReclaimed", (ev) => {
-                    return ev.sender === sender && testAmount.eq(ev.amount) && ev.retrievalCode === retrievalCodeSecure;
+                    return ev.sender === sender && testAmount.eq(ev.amount) && ev.retrievalCodeSecure === retrievalCodeSecure;
                 });
             });
 
-            it("should provide the same retrieval code in web3 and contract for senders", async function () {
-                assert.strictEqual(retrievalCodeSecure, await remi.createRetrievalCodeSecure("Part One", "Part Two", exchange), "Secure retrieval code version for sender created by web3 and contract don't match.");
+
+            it("should allow extension of the expire Timestamp", async function () {
+                let remiPayment = await remi.payments.call(retrievalCodeSecure);
+                let extensionSeconds = new BN(100);
+
+                let remiExpireTimeTxObj = await remi.expireTimeExtend(retrievalCodeSecure, extensionSeconds, {from: sender});
+
+                let remiPaymentUpdated = await remi.payments.call(retrievalCodeSecure);
+
+                assert.strictEqual(remiPayment.expiresTimestamp.add(extensionSeconds).toString(), remiPaymentUpdated.expiresTimestamp.toString(), "Timestamps should be correctly updated.");
+                assert.strictEqual(remiExpireTimeTxObj.logs.length, 1, "Only one event is allowed in this transaction.");
+
+                await truffleAssert.eventEmitted(remiExpireTimeTxObj, "LogTimestampExtended", (ev) => {
+                    return ev.sender === sender && remiPaymentUpdated.expiresTimestamp.eq(ev.expiresTimestamp) && ev.retrievalCodeSecure === retrievalCodeSecure;
+                });
             });
 
-            it("should provide the same retrieval code in web3 and contract for exchanges", async function () {
-                assert.strictEqual(retrievalCode, await remi.createRetrievalCode("Part One", "Part Two"), "Retrieval code version for exchange created by web3 and contract don't match.");
+            it("should allow shortening of the expire Timestamp", async function () {
+                let remiPayment = await remi.payments.call(retrievalCodeSecure);
+                let shortenSeconds = new BN(100);
+
+                let remiShortenTimeTxObj = await remi.expireTimeShorten(retrievalCode, shortenSeconds, {from: exchange});
+
+                let remiPaymentUpdated = await remi.payments.call(retrievalCodeSecure);
+
+                assert.strictEqual(remiPayment.expiresTimestamp.sub(shortenSeconds).toString(), remiPaymentUpdated.expiresTimestamp.toString(), "Timestamps should be correctly updated.");
+                assert.strictEqual(remiShortenTimeTxObj.logs.length, 1, "Only one event is allowed in this transaction.");
+
+                await truffleAssert.eventEmitted(remiShortenTimeTxObj, "LogTimestampShortend", (ev) => {
+                    return ev.sender === exchange && remiPaymentUpdated.expiresTimestamp.eq(ev.expiresTimestamp) && ev.retrievalCodeSecure === retrievalCodeSecure;
+                });
             });
+
         });
 
         describe("Withdraw fail cases", function () {
@@ -131,23 +181,89 @@ contract("Remittance", accounts => {
                     remi.withdraw(retrievalCodeWrong, {from: exchange})
                 )
             });
-        })
+
+
+            it("should not allow withdrawals that expired", async function () {
+                let timeDiff = testTimestampFuture.mul(new BN(1000)).sub(new BN(Date.now())).add(new BN(2000));
+                if (timeDiff.gt(new BN(0))) {
+                    await new Promise(resolve => {
+                        setTimeout(resolve, timeDiff);
+                    });
+                    let currentBlock = await web3.eth.getBlockNumber();
+
+                    while (currentBlock >= await web3.eth.getBlockNumber()) {
+                        setTimeout(function () {
+                        }, 1000);
+                    }
+                }
+                await truffleAssert.fails(
+                    remi.withdraw(retrievalCode, {from: exchange})
+                )
+            });
+        });
+
+        describe("Reclaim fail cases", function () {
+            it("should not allow reclaims that are not due", async function () {
+
+                assert.isTrue((new BN(Date.now())).div(new BN(1000)).lt(testTimestampFuture), "Test condition not met: Future Timestamp has already expired");
+                await truffleAssert.fails(
+                    remi.reclaim(retrievalCodeSecure, {from: sender})
+                )
+            });
+        });
+
+        describe("Expired Timestamps fail cases", function () {
+
+            it("should not allow extension of the expire Timestamp beyond the limit", async function () {
+                let extensionSeconds = new BN(10100);
+
+                await truffleAssert.fails(
+                    remi.expireTimeExtend(retrievalCodeSecure, extensionSeconds, {from: sender})
+                )
+            });
+
+            it("should not allow extension of the expire Timestamp by the exchange", async function () {
+                let extensionSeconds = new BN(100);
+
+                await truffleAssert.fails(
+                    remi.expireTimeExtend(retrievalCodeSecure, extensionSeconds, {from: exchange})
+                )
+            });
+
+            it("should not allow shortening of the expire Timestamp beyond  now", async function () {
+                let shortenSeconds = (new BN(Date.now()).div(new BN(1000)).add(new BN(20000)));
+
+                await truffleAssert.fails(
+                    remi.expireTimeShorten(retrievalCodeSecure, shortenSeconds, {from: exchange})
+                )
+            });
+
+            it("should not allow shortening of the expire Timestamp by the payment sender", async function () {
+                let shortenSeconds = new BN(20000);
+
+                await truffleAssert.fails(
+                    remi.expireTimeShorten(retrievalCodeSecure, shortenSeconds, {from: sender})
+                )
+            });
+
+        });
     });
 
     describe("Deposit fail cases", function () {
 
-        it("should not accept deposits without a recipient", async function () {
+        it("should not accept deposits without a expiryTime", async function () {
             await truffleAssert.fails(
-                remi.deposit(retrievalCodeSecure, "0x0", {from: sender, value: testAmount})
+                remi.deposit(retrievalCodeSecure, "0", {from: sender, value: testAmount})
             )
         });
 
         it("should not accept deposits without a retrieval code", async function () {
             await truffleAssert.fails(
-                remi.deposit("", exchange, {from: sender, value: testAmount})
+                remi.deposit("", new BN(Date.now()).div(new BN(1000)).add(new BN(120)), {from: sender, value: testAmount})
             )
         });
     });
-
-
 });
+
+
+
